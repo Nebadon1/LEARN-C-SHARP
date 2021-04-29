543fe8e1f5b16205b199d8a97e83ffea2ebcacfb
Carlos Mitchell <kakismitchell@gmail.com>
2021-04-28 22:28:31 -0400

    arrays

---
+++ Arrarys/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Arrarys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckynumbers = {3, 4, 34, 5, 19};
+            string[] friends = new string[5];
+            friends[0] = "Carlos";
+            Console.WriteLine( luckynumbers[0]);
+            Console.WriteLine(friends[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
