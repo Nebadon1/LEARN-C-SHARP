9665e69a501f7148f00ad7987ebfc2aedbfa31ec
Carlos Mitchell <kakismitchell@gmail.com>
2021-04-28 21:58:43 -0400

    added

---
+++ Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color =Console.ReadLine();
+            Console.Write("Enter a  noun: ");
+            pluralNoun = Console.ReadLine();
+            Console.Write("Enter a Celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are "+ color);
+            Console.WriteLine( pluralNoun + "are blue");
+            Console.WriteLine("I love " + celebrity);
+
+        }
+    }
+}
